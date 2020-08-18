771fd0446fcf78aa1bf2141e31ca1346bc20ab2f
André Marco Ruiz Sandoval <0204571@up.edu.mx>
2020-08-18 13:12:34 -0500

    social icons added

---
+++ src/components/AboutUs/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../../assets/style/AboutUs/Hero.scss';
+
+const Hero = () => {
+  return (
+    <div className="about-us">
+      <h1>URBAFIX</h1>
+      <div className="container-text">
+        <p>
+          Somos una empresa 100% mexicana, especializada en productos y
+          servicios de alto rendimiento en la industria de señalización,
+          seguridad vial y mobiliario urbano.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
