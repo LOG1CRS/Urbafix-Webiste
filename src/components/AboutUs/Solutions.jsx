1997e6871b1b0ba59b795421778c560e6dda56d4
André Marco Ruiz Sandoval <log1c.rs.psc@gmail.com>
2020-10-05 11:29:06 -0500

    Typewritter effect added

--- src/components/AboutUs/Solutions.jsx
+++ src/components/AboutUs/Solutions.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 import '../../assets/style/AboutUs/Solutions.scss';
 
@@ -7,11 +9,20 @@ const Solutions = () => {
   return (
     <div className="solutions">
       <div className="solutions-container">
-        <h4 data-aos="zoom-in" data-aos-easing="linear">
-          SOLUCIONES <strong>MODERNAS</strong> CON ALTOS ESTÁNDARES DE
-          <strong>CALIDAD</strong>.
+        <h4>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(
+                  'SOLUCIONES MODERNAS CON ALTOS ESTÁNDARES DE CALIDAD'
+                )
+                .start();
+            }}
+          />
+          {/* SOLUCIONES <strong>MODERNAS</strong> CON ALTOS ESTÁNDARES DE
+          <strong> CALIDAD</strong>. */}
         </h4>
-        <p data-aos="fade-up">
+        <p data-aos="fade-zoom-in" data-aos-offset="0" data-aos-delay="300">
           Buscamos y creamos proyectos de carácter contemporáneo, de acuerdo a
           las normas de seguridad vial internacionales y con una estética
           moderna acorde a los tiempos que vivimos.
--- src/components/Main/Statistics.jsx
+++ src/components/Main/Statistics.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
+import Typewriter from 'typewriter-effect';
 
 import '../../assets/style/Main/Statistics.scss';
 
@@ -9,12 +11,23 @@ const Statistics = () => {
   return (
     <div className="statistics">
       <div className="statistics-phrase">
-        <h4 data-aos="fade-zoom-in" data-aos-offset="0" data-aos-delay="300">
-          “NUESTRO TRABAJO ES EL REFLEJO DE LA
+        <h4>
+          <VisibilitySensor onChange={(typewriter) => typewriter.start}>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(
+                    '“NUESTRO TRABAJO ES EL REFLEJO DE LA MODERNIZACIÓN DEL PAISAJE URBANO”'
+                  )
+                  .start();
+              }}
+            />
+          </VisibilitySensor>
+          {/* “NUESTRO TRABAJO ES EL REFLEJO DE LA
           <br />
           <strong>MODERNIZACIÓN </strong>
           DEL
-          <strong> PAISAJE URBANO</strong>”
+          <strong> PAISAJE URBANO</strong>” */}
         </h4>
       </div>
       <div className="statistics-numbs">
