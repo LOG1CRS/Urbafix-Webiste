d9f6660e5ed376672760e43b8d2b5dac5d5bd1f6
André Marco Ruiz Sandoval <log1c.rs.psc@gmail.com>
2020-10-08 16:39:22 -0500

    Whatsapp float button added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { whatsapp } from '../../routes/routes.json';
+
+import '../../assets/style/Layout/FloatButton.scss';
 
 const Layout = ({ children }) => {
+  const handlerTooltip = () => {
+    const tooltip = document.getElementById('tooltip');
+    console.log(tooltip);
+  };
+
+  useEffect(() => {
+    handlerTooltip();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       {children}
+      <div id="tooltip">
+        <a
+          href={whatsapp}
+          target="_blank"
+          rel="noreferrer"
+          className="float-button"
+        >
+          <i className="fab fa-whatsapp icon" />
+        </a>
+      </div>
       <Footer />
     </div>
   );
